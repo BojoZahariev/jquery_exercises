787efb08bb7cb4ebe0ab5bdfb7745c9550b9709a
Bojo <bozhidar_zahariev@hotmail.com>
2020-11-23 10:39:49 +0000

    animation

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ $(document).ready(function() {
   });
 });
 
+//FADE
+
 $('button').click(function() {
   $('#div1').fadeIn();
   $('#div2').fadeIn('slow');
@@ -37,4 +39,41 @@ $('button').click(function() {
   $('#div1').fadeOut();
   $('#div2').fadeOut('slow');
   $('#div3').fadeOut(3000);
+});
+
+$('button').click(function() {
+  $('#div1').fadeTo('slow', 0.15);
+  $('#div2').fadeTo('slow', 0.4);
+  $('#div3').fadeTo('slow', 0.7);
+});
+
+//SLIDE
+
+$('#flip').click(function() {
+  $('#panel').slideDown();
+});
+
+$('#flip').click(function() {
+  $('#panel').slideToggle();
+});
+
+//ANIMATE
+
+$('button').click(function() {
+  $('div').animate({
+    left: '250px',
+    opacity: '0.5',
+    height: '150px',
+    width: '150px'
+  });
+});
+
+//Uses Queue
+
+$('button').click(function() {
+  var div = $('div');
+  div.animate({ height: '300px', opacity: '0.4' }, 'slow');
+  div.animate({ width: '300px', opacity: '0.8' }, 'slow');
+  div.animate({ height: '100px', opacity: '0.4' }, 'slow');
+  div.animate({ width: '100px', opacity: '0.8' }, 'slow');
 });
