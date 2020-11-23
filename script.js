e92e5f609c71ae6e8fa3b1cda3c66eba4bc2c3b9
Bojo <bozhidar_zahariev@hotmail.com>
2020-11-23 10:05:37 +0000

    fade in/out

--- script.js
+++ script.js
@@ -12,9 +12,29 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#hide').click(function() {
-    $('p').hide();
+    //slow hide, milliseconds or "slow"
+    $('p').hide(1000);
   });
+
   $('#show').click(function() {
     $('p').show();
   });
+});
+
+$(document).ready(function() {
+  $('button').click(function() {
+    $('p').toggle();
+  });
+});
+
+$('button').click(function() {
+  $('#div1').fadeIn();
+  $('#div2').fadeIn('slow');
+  $('#div3').fadeIn(3000);
+});
+
+$('button').click(function() {
+  $('#div1').fadeOut();
+  $('#div2').fadeOut('slow');
+  $('#div3').fadeOut(3000);
 });
